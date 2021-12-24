367674dd966f33ef3def07d84fc365797926395e
saharoori <saharlaor@gmail.com>
2021-12-24 23:59:51 +0200

    Header component routing to other components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
-  return <div className="Header"></div>;
+  return (
+    <div className="Header">
+      <Link to="/">Profile</Link>
+      <Link to="/training">Training</Link>
+    </div>
+  );
 }
 
 export default Header;
