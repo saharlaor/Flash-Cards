19ba1413e24800d423eb68dcf336eb54b44881b4
saharoori <saharlaor@gmail.com>
2021-12-24 21:59:35 +0200

    React router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" exact component={App} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
